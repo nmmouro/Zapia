14c76fa1b19eccfa9e16ad23ee9636a4ac8caad7
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-29 11:12:51 -0300

    Create veiculos.helpers.js

---
+++ js/utils/veiculos.helpers.js
@@ -0,0 +1,28 @@
+// ============================================================================
+// DATA AUTOMÁTICA
+// ============================================================================
+
+export function preencherDataAtual(){
+
+    if (!campoData) {
+
+        return;
+    }
+
+    campoData.value =  dataInput();
+}
+
+// ============================================================================
+// TRATAMENTO DE ERROS
+// ============================================================================
+
+export function tratarErro(){
+
+    erro
+
+) {
+    console.error(erro);
+
+    alert(erro?.message ||  "Erro ao processar veículo.");
+
+}
