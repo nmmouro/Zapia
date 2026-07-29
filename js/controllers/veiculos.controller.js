94d12d3ab7bca327828c53e015d3b42293385b25
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-29 10:29:00 -0300

    Add veiculos.controller.js for vehicle management

---
+++ js/controllers/veiculos.controller.js
@@ -0,0 +1,67 @@
+import {
+    carregarTabela,
+    editarVeiculo,
+    removerVeiculo
+} from "../tables/veiculos.table.js";
+
+import {
+    salvarFormulario,
+    novoFormulario
+} from "../forms/veiculos.form.js";
+
+import {
+    preencherDataAtual,
+    tratarErro
+} from "../utils/veiculos.helpers.js";
+
+import {
+    mostrarLoading,
+    esconderLoading
+} from "../ui/loading.js";
+
+export async function initVeiculos(){
+
+    try{
+
+        mostrarLoading();
+
+        preencherDataAtual();
+
+        registrarEventos();
+
+        await carregarTabela();
+
+    }
+    catch(erro){
+
+        tratarErro(erro);
+
+    }
+    finally{
+
+        esconderLoading();
+
+    }
+
+}
+
+function registrarEventos(){
+
+    document
+        .querySelector("#formveiculo")
+        ?.addEventListener(
+            "submit",
+            salvarFormulario
+        );
+
+    document
+        .querySelector("#btnNovo")
+        ?.addEventListener(
+            "click",
+            novoFormulario
+        );
+
+}
+
+window.editarVeiculo = editarVeiculo;
+window.removerVeiculo = removerVeiculo;
