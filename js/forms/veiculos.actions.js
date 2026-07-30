390af8ac8af0cb65f648efa2044849ad42f74965
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-30 09:43:18 -0300

    Add veiculos.actions.js for vehicle form handling

---
+++ js/forms/veiculos.actions.js
@@ -0,0 +1,89 @@
+import {
+
+    salvarVeiculo,
+    atualizarVeiculo
+
+} from "../services/veiculos.js";
+
+import {
+
+    mostrarLoading,
+    esconderLoading
+
+} from "../ui/loading.js";
+
+import {
+
+    tratarErro
+
+} from "../utils/veiculos.helpers.js";
+
+import {
+
+    obterDadosFormulario
+
+} from "./veiculos.fields.js";
+
+import {
+
+    carregarTabela
+
+} from "../controllers/veiculos.controller.js";
+
+import {
+
+    preencherDataAtual
+
+} from "../utils/datas.js";
+
+let registroEditando = null;
+
+export function setRegistroEditando(id) {
+
+    registroEditando = id;
+
+}
+
+export async function salvarFormulario(evento, formulario) {
+
+    evento.preventDefault();
+
+    try {
+
+        mostrarLoading();
+
+        const dados =
+            obterDadosFormulario(formulario);
+
+        if (registroEditando) {
+
+            await atualizarVeiculo(
+                registroEditando,
+                dados
+            );
+
+        } else {
+
+            await salvarVeiculo(dados);
+
+        }
+
+        limparFormulario(formulario);
+
+        await carregarTabela();
+
+    }
+
+    catch (erro) {
+
+        tratarErro(erro);
+
+    }
+
+    finally {
+
+        esconderLoading();
+
+    }
+
+}
