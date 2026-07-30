d90668afdde8d030c30a524bb865ee14f7c2fc64
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-30 09:44:35 -0300

    Refactor form functions for vehicle management
    Refactor form handling by removing the 'salvarFormulario' function and integrating 'limparFormulario' and 'novoFormulario' functionalities.

--- js/forms/veiculos.form.js
+++ js/forms/veiculos.form.js
@@ -1,107 +1,18 @@
-export async function salvarFormulario(evento){
+export function limparFormulario(formulario) {
 
-    evento.preventDefault();
-
-    try {
-
-        mostrarLoading();
-
-        const dados =  obterDadosFormulario();
-       
-        if (registroEditando) {
-
-            await atualizarVeiculo(registroEditando, dados);
-        }
- 
-        else {
-
-            await salvarVeiculo(dados);
-        }
-
-        formulario.reset();
-
-        preencherDataAtual();
-
-        registroEditando =  null;
-
-        document.body.classList.remove("modo-edicao");
-
-        await carregarTabela();
-    }
-
-    catch (erro) {
-
-        tratarErro(erro);
-    }
-
-    finally {
-      
-        esconderLoading();
-    }
-}
-
-  
-export function novoFormulario(){
-
-    registroEditando =  null;
+    registroEditando = null;
 
     formulario.reset();
 
     preencherDataAtual();
 
     document.body.classList.remove("modo-edicao");
-}
-
-
-function obterDadosFormulario(){
-
-    return {
-
-        Data:
-            formulario.elements["data"].value,
-
-        Placa:
-            formulario.elements["placa"].value.trim(),
-
-        Modelo:
-            formulario.elements["modelo"].value.trim(),
-
-        Marca:
-            formulario.elements["marca"].value.trim(),
-
-        Ano:
-            formulario.elements["ano"].value,
-
-        Cor:
-            formulario.elements["cor"].value.trim(),
-
-        Combustível:
-            formulario.elements["combustivel"].value,
-
-        Status:
-            formulario.elements["status"].value
-
-    };
 
 }
-  
 
-export function preencherFormulario(veiculo){
 
-    campoData.value = dataParaInput(veiculo["Data"]) || "";
+export function novoFormulario(formulario) {
 
-    campoPlaca.value = veiculo["Placa"]  || "";
-
-   campoModelo.value = veiculo["Modelo"] || "";
-    
-    campoMarca.value = veiculo["Marca"] || "";
-
-    campoAno.value = veiculo["Ano"] || "";
-
-    campoCor.value = veiculo["Cor"] || "";
-
-    campoCombustivel.value = veiculo["Combustível"] || "";
-
-    campoStatus.value = veiculo["Status"]  || "";
+    limparFormulario(formulario);
 
 }
