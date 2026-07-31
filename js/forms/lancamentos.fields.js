cc1ee979602a93be5c37251d44fd92253e0581cd
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-30 21:51:55 -0300

    Create lancamentos.fields.js

---
+++ js/forms/lancamentos.fields.js
@@ -0,0 +1,71 @@
+import { dataParaInput } from "../utils/datas.js";
+
+
+function obterDadosFormulario(formulario) {
+
+    return {
+
+        Data:  campoData.value,
+
+        Hora:  campoHora.value,
+
+        "Empregado / Matrícula":
+            selectEmpregado.value,
+
+        Veículo:
+            selectVeiculo.value,
+
+        "Passageiro / Setor / Motivo":
+            [
+                formulario.passageiro?.value,
+                formulario.setor?.value,
+                formulario.motivo?.value
+            ]
+            .filter(Boolean)
+            .join(" / "),
+
+        Itinerário:
+            formulario.itinerario?.value || "",
+
+        Status:
+            formulario.status.value
+        
+    };
+
+}
+
+
+export function preencherFormulario(registro){
+
+
+    campoData.value = dataParaInput(registro["Data"]) || "";
+
+
+  
+    campoHora.value = horaParaInput(registro["Hora"]) || "";
+
+
+  selectEmpregado.value = registro["Empregado / Matrícula"] || "";
+    
+
+  selectVeiculo.value = registro["Veículo"] || "";
+
+   
+    const partes = String(registro["Passageiro / Setor / Motivo"]  || "").split(" / ");
+
+
+    formulario.passageiro.value = partes[0] || "";
+
+
+    formulario.setor.value = partes[1] || "";
+
+
+    formulario.motivo.value = partes[2] || "";
+
+   
+    formulario.itinerario.value = registro["Itinerário"] || "";
+
+   
+    formulario.status.value = registro["Status"] || "";
+
+}
