a0f2971867e4af5c99b4f6b456ea6be690bb43cb
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-31 16:05:27 -0300

    Update lancamentos.js

--- js/pages/lancamentos.js
+++ js/pages/lancamentos.js
@@ -3,9 +3,34 @@
 // Arquivo: js/pages/lancamentos.js
 // ============================================================================
 
-import { initLancamentos } from "../controllers/lancamentos.controller.js";
+import {
+    carregarTabela
+} from "../controllers/lancamentos.controller.js";
 
-document.addEventListener(
-    "DOMContentLoaded",
-    initLancamentos
-);
+
+import {
+    salvarFormulario,
+    novoFormulario
+} from "../forms/lancamentos.form.js";
+
+
+    document.addEventListener("DOMContentLoaded", async () => {
+
+    const formulario = document.querySelector("#formlancamentos");
+    const btnNovo = document.querySelector("#btnNovo");
+
+    formulario.addEventListener("submit", (evento) => {
+
+    salvarFormulario(evento, formulario);
+
+});
+
+btnNovo.addEventListener("click", () => {
+
+    novoFormulario(formulario);
+
+});
+
+    await carregarTabela();
+
+});
