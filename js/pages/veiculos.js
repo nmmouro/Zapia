cc4aae3abb67e7033962a42db16304c59f945ad1
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-07-31 15:47:50 -0300

    Refactor event listener for DOMContentLoaded

--- js/pages/veiculos.js
+++ js/pages/veiculos.js
@@ -7,17 +7,19 @@ import {
     carregarTabela
 } from "../controllers/veiculos.controller.js";
 
+
 import {
     salvarFormulario,
     novoFormulario
 } from "../forms/veiculos.form.js";
 
-document.addEventListener("DOMContentLoaded", async () => {
+
+    document.addEventListener("DOMContentLoaded", async () => {
 
     const formulario = document.querySelector("#formveiculo");
-const btnNovo = document.querySelector("#btnNovo");
+    const btnNovo = document.querySelector("#btnNovo");
 
-formulario.addEventListener("submit", (evento) => {
+    formulario.addEventListener("submit", (evento) => {
 
     salvarFormulario(evento, formulario);
 
