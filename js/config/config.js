efb70c55cbd1e7c21e0d87e1ddb27b2055e60478
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-01 14:44:17 -0300

    Update config.js

--- js/config/config.js
+++ js/config/config.js
@@ -15,7 +15,7 @@ export const CONFIG = {
 
     API_URL:
 
-        "https://script.google.com/macros/s/AKfycbxHJgtTWHi5bbzrbQfOJkLNTF-XkUqGrC0CGyG4aL177nW-p0PKwgSbaw2JkduD11Fb7A/exec",
+        "https://script.google.com/macros/s/AKfycbzwyrOh5l5SpSuVNDkzBqbmruSoA14hYFPb0DsG2vhTYAqeb2VpFZPOsnKwrbgXxfyNcQ/exec",
 
     UPDATE_INTERVAL: 30000,
 
