37e65b8735ac4d7130882448bb38efab70364c1c
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-02 14:20:58 -0300

    Create empregados.state.js

---
+++ js/controllers/empregados.state.js
@@ -0,0 +1,77 @@
+// ============================================================================
+// EMPREGADOS - STATE
+// Painel Frota
+// Arquivo: js/controllers/empregados.state.js
+//
+// Responsável pelos elementos do formulário de empregados.
+// ============================================================================
+
+
+// ============================================================================
+// ELEMENTOS DO FORMULÁRIO
+// ============================================================================
+
+
+export const formulario =
+
+    document.querySelector("#formEmpregado");
+
+
+
+export const campoData =
+
+    document.querySelector("#data");
+
+
+
+export const campoHora =
+
+    document.querySelector("#hora");
+
+
+
+export const campoFoto =
+
+    document.querySelector("#foto");
+
+
+
+export const campoEmpregado =
+
+    document.querySelector("#empregado");
+
+
+
+export const campoMatricula =
+
+    document.querySelector("#matricula");
+
+
+
+export const campoDiretoria =
+
+    document.querySelector("#diretoria");
+
+
+
+export const campoSetor =
+
+    document.querySelector("#setor");
+
+
+
+export const campoUsuario =
+
+    document.querySelector("#usuario");
+
+
+
+export const campoCondicao =
+
+    document.querySelector("#condicao");
+
+
+
+export const campoStatus =
+
+    document.querySelector("#status");
