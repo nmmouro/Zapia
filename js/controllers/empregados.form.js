7a2393f67698ff38a24fdd9d401580d1cada7176
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-02 21:38:42 -0300

    Update empregados.form.js

--- js/controllers/empregados.form.js
+++ js/controllers/empregados.form.js
@@ -42,8 +42,8 @@ import {
 
 import {
 
-    registroEditando,
-    definirRegistroEditando,
+    getRegistroEditando,
+    setRegistroEditando,
     tituloFormulario
 
 } from "./empregados.state.js";
