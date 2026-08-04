6b1344086ff9f16dfa170f95b4918ee67f0a934e
nmouro <32180968+nmmouro@users.noreply.github.com>
2026-08-04 11:40:40 -0300

    Add dashboard occurrences configuration file

---
+++ js/config/tabelas/dashboard.ocorrencias.js
@@ -0,0 +1,65 @@
+// ============================================================================
+// DASHBOARD - OCORRÊNCIAS
+// Painel Frota
+// Arquivo: js/config/tabelas/dashboard.ocorrencias.js
+// ============================================================================
+
+export const COLUNAS_DASHBOARD_OCORRENCIAS = [
+
+    {
+
+        key: "Data",
+
+        label: "Data"
+
+    },
+
+    {
+
+        key: "Hora Inicial",
+
+        label: "Hora"
+
+    },
+
+    {
+
+        key: "Veículo",
+
+        label: "Veículo"
+
+    },
+
+    {
+
+        key: "Empregado",
+
+        label: "Empregado"
+
+    },
+
+    {
+
+        key: "Destino",
+
+        label: "Destino"
+
+    },
+
+    {
+
+        key: "Tempo",
+
+        label: "Tempo"
+
+    },
+
+    {
+
+        key: "Status",
+
+        label: "Status"
+
+    }
+
+];
